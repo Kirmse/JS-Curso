20268f18e6c85cd1df64e89724bcf81bcdbbfb25
Kirmse_. <kirmse2005@gmail.com>
2023-09-14 23:12:12 -0300

    ModuloD

---
+++ curso/ModuloD/008.js
@@ -0,0 +1,6 @@
+var vel = 60.6
+console.log(`a velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida`)
+}
+console.log(`Se beber, dirija!`)
