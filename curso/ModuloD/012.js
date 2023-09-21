e63af6289f6cebfea46cd8a23bd10ae0b0a79f2e
Kirmse_. <kirmse2005@gmail.com>
2023-09-20 23:37:05 -0300

    ModuloD

---
+++ curso/ModuloD/012.js
@@ -0,0 +1,24 @@
+// var idade = 70
+// if (idade < 16) {
+//     console.log('Não vota')
+// } else if (idade < 18 || idade >= 65) {
+//     console.log('Voto opcional')
+// }else{
+//     console.log('Voto obrigatório')
+// }
+
+
+var hora = 24
+
+if (hora > 0 && hora < 25) {
+console.log(`Agora são exatamente ${hora} horas.`)
+    if (hora < 12) {
+        console.log('Bom dia!')
+    } else if (hora < 18) {
+        console.log('Boa tarde.')
+    }else{
+        console.log('Boa noite.')
+    }
+} else {
+    console.log('Hora incorreto!')
+}
