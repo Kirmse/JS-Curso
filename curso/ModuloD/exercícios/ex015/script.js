5f379499d2e6e4dc440f15a5fab184811b334a7c
Kirmse_. <kirmse2005@gmail.com>
2023-10-10 23:04:46 -0300

    ModuloD

--- curso/ModuloD/exercícios/ex015/script.js
+++ curso/ModuloD/exercícios/ex015/script.js
@@ -1,1 +1,37 @@
-alert('Hello!')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var formularioAno = Number(document.getElementById('txtano').value)
+    var res = document.querySelector('div#res')
+    var img = document.querySelector('img#imagem')
+    
+    anoCalculo = ano - 120
+
+    if( formularioAno >= anoCalculo && formularioAno <= ano) {
+        var formularioSexo = document.getElementsByName('radsex')
+        var idade = ano - formularioAno
+        var genero = ''
+        
+        if (formularioSexo[0].checked) {
+            genero = 'Homem'
+        } else if (formularioSexo[1].checked) {
+            genero = 'Mulher'
+        }
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `Voce é ${genero} e tem ${idade} anos`
+
+        if (idade >=0 && idade <= 18){
+
+        }else if (idade <= 50){
+
+        }else{
+
+        }
+
+    }else{
+        window.alert('inválido')
+    }
+} 
+
+// https://www.cursoemvideo.com/curso/javascript/aulas/condicoes-em-javascript/modulos/exercicios-javascript-parte-3/ 15:00
