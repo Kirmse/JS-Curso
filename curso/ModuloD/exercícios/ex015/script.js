6970bc841170603ae8d2b0f300d98c703ec5ff0a
Kirmse_. <kirmse2005@gmail.com>
2023-10-11 18:55:44 -0300

    ModuloD

--- curso/ModuloD/exercícios/ex015/script.js
+++ curso/ModuloD/exercícios/ex015/script.js
@@ -3,10 +3,10 @@ function verificar() {
     var ano = data.getFullYear()
     var formularioAno = Number(document.getElementById('txtano').value)
     var res = document.querySelector('div#res')
-    
+
     anoCalculo = ano - 120
 
-    if( formularioAno >= anoCalculo && formularioAno <= ano) {
+    if (formularioAno >= anoCalculo && formularioAno <= ano) {
         var formularioSexo = document.getElementsByName('radsex')
         var idade = ano - formularioAno
         var genero = ''
@@ -14,49 +14,70 @@ function verificar() {
         img.setAttribute('id', 'foto')
 
         if (formularioSexo[0].checked) {
-            genero = 'Homem'
-            
-            if (idade >= 0 && idade <14 ){ 
+            genero = 'homem'
+
+            if (idade >= 0 && idade < 14) {
                 //CRIANÇA
                 img.setAttribute('src', 'homemcrianca.png')
-            }else if (idade <= 21){
+            } else if (idade <= 21) {
                 //JOVEM
                 img.setAttribute('src', 'homemjovem.png')
-            }else if (idade <= 50){
+            } else if (idade <= 50) {
                 //ADULTO
-                img.setAttribute('src', 'homemadulto.png' )
-            }else{
+                img.setAttribute('src', 'homemadulto.png')
+            } else {
                 //IDOSO
                 img.setAttribute('src', 'homemidoso.png')
             }
 
         } else if (formularioSexo[1].checked) {
-            genero = 'Mulher'
+            genero = 'mulher'
 
-            if (idade >= 0 && idade <14 ){ 
+            if (idade >= 0 && idade < 14) {
                 //CRIANÇA
                 img.setAttribute('src', 'mulhercrianca.png')
-            }else if (idade <= 21){
+            } else if (idade <= 21) {
                 //JOVEM
                 img.setAttribute('src', 'mulherjovem.png')
-            }else if (idade <= 50){
+            } else if (idade <= 50) {
                 //ADULTO
-                img.setAttribute('src', 'mulheradulta.png' )
-            }else{
+                img.setAttribute('src', 'mulheradulta.png')
+            } else {
                 //IDOSO
                 img.setAttribute('src', 'mulheridosa.png')
             }
 
         }
-        
-        res.style.textAlign = 'center'
-        res.innerHTML = `Voce é ${genero} e tem ${idade} anos`
-        res.appendChild(img)
-        
 
-    }else{
+        res.style.textAlign = 'center'
+        res.innerHTML = `Voce é <strong>${genero}</strong> e tem <strong>${idade} anos</strong>.`
+        res.appendChild(img)
+
+
+    } else {
         window.alert('inválido')
     }
-} 
+}
 
-// https://www.cursoemvideo.com/curso/javascript/aulas/condicoes-em-javascript/modulos/exercicios-javascript-parte-3/ 15:00
+// --------------------- BUTÃO ----------------------------
+
+var mouse = window.document.getElementById('butao')
+
+mouse.addEventListener('mouseenter', entrar)
+mouse.addEventListener('mouseout', sair)
+mouse.addEventListener('click', clicar)
+mouse.addEventListener('mousedown', pressionar)
+
+
+function entrar() {
+    document.getElementById('butao').style.backgroundColor = 'green'
+}
+function sair() {
+    mouse.style.backgroundColor = 'rgb(0, 193, 0)'
+}
+function clicar() {
+    mouse.style.backgroundColor = 'rgb(0, 230, 0)'
+}
+function pressionar() {
+    document.getElementById('butao').style.backgroundColor = 'rgb(0, 230, 0)'
+}
