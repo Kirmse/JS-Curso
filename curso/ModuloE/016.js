350f65c8321fcc3efbc6be48b08d63dd1d544cf1
Kirmse_. <kirmse2005@gmail.com>
2023-10-17 19:18:19 -0300

    ModuloE

---
+++ curso/ModuloE/016.js
@@ -0,0 +1,30 @@
+// Repetições
+
+var c = 1
+
+
+// Do ---------------------------------------------------
+
+do {
+    console.log(`Tudo bem? ${c}`)
+    c++  // c = c + 1
+} while (c <= 100)
+
+
+// Enquanto ---------------------------------------------------
+
+// while (c <= 100) {
+//     console.log(`Tudo bem? ${c}`)
+//     c++  // c = c + 1
+// }
+
+// manual ---------------------------------------------------
+
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
+// console.log('Tudo bem?')
