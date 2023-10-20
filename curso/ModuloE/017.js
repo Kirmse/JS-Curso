aa98e1ab2dc43cf16a03fbe83a64ee9057970838
Kirmse_. <kirmse2005@gmail.com>
2023-10-20 17:30:39 -0300

    ModuloE

---
+++ curso/ModuloE/017.js
@@ -0,0 +1,19 @@
+// Repetições 2 
+
+var c = 1
+
+console.log('vai começar a contagem')
+
+while (c <= 10) {
+    console.log(`While ${c}`)
+    c++
+}
+
+console.log('terminou')
+console.log('vamos fazer outra')
+
+for (var d = 1; d <= 10; d++) {
+    console.log(`For ${d}`)
+}
+
+console.log('acabou')
